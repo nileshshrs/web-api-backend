9f931e3d39c20fd119f3b185e6ae32c7243b5c54
nileshshrs <nileshshr54@gmail.com>
2024-11-30 10:25:37 +0545

    changed user profile route name

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,6 @@ import { Router } from "express"
 import { userController } from "../controllers/userController.js"
 
 const userRoutes = Router()
-userRoutes.get('/get', userController)
+userRoutes.get('/profile', userController)
 
 export default userRoutes
